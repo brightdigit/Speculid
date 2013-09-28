573731d4ab57a1c60b2eea36d6ac991308536e69
Leo G Dion <leogdion@brightdigit.com>
2013-09-28 18:41:53 -0400

    failing tests

--- test/controllers/account.js
+++ test/controllers/account.js
@@ -1,37 +1,46 @@
-var proxyquire =  require('proxyquire');
+var proxyquire = require('proxyquire');
 var foo = proxyquire('../../app/controllers/Account.js', {});
 
 exports.Register = {
-	testValid :
-		function(test){
-		    var request = {
-		    	body : {
-		    		emailAddress : 'example@valid.com'
-		    	}
-		    };
+  testValid: function (test) {
+    var request = {
+      body: {
+        emailAddress: 'example@valid.com'
+      }
+    };
     test.ok(false, "this assertion should fail");
     test.done();
+  },
+  testAlreadyExists: function (test) {
+    test.ok(false, "this assertion should fail");
+    test.done();
+  },
+  testInvalidEmailAddress: function (test) {
+    test.ok(false, "this assertion should fail");
+    test.done();
+  },
+  testPropertyExists: function (test) {
+    test.ok(false, "this assertion should fail");
+    test.done();
+  },
+};
+
+exports.Confirm = {
+  test : function (test) {
+    test.ok(false, "test failed.");
+    test.done();
+  }
+};
 
 
-		},
-	testAlreadyExists : 
-		function(test) {
-    test.ok(false, "this assertion should fail");
+exports.Login = {
+  test : function (test) {
+    test.ok(false, "test failed.");
     test.done();
-		},
-	testInvalidEmailAddress :
-		function (test) {
-    test.ok(false, "this assertion should fail");
-    test.done();
-		},
-	testPropertyExists : 
-		function (test) {
-    test.ok(false, "this assertion should fail");
-    test.done();
-		},
-}
+  }
+};
 
-exports.testSomethingElse = function(test){
-    test.ok(false, "this assertion should fail");
-    test.done();
+exports.testSomethingElse = function (test) {
+  test.ok(false, "this assertion should fail");
+  test.done();
 };
