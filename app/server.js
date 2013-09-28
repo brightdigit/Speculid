4a425f8f0a93412b939d042bebad4c09c040697b
leogdion <leogdion@brightdigit.com>
2013-09-27 20:13:00 -0400

    working on registration

--- app/server.js
+++ app/server.js
@@ -1,16 +1,59 @@
-// Load the http module to create an http server.
-var http = require('http');
+/*
+var express = require('express');
+var app = express();
 
-var port = process.env.PORT || 9000;
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+app.post('/api/v1/register', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(port);
+app.post('/api/v1/confirm', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
 
-// Put a friendly message on the terminal
-console.log("Server running on port: " + port);
+app.listen(3000);
+console.log('Listening on port 3000');
+*/
+
+/*
+ set mailserver smtp.gmail.com port 587
+    username "monit@brightdigit.com" password "monit5ervices"
+    using tlsv1
+*/
+
+var nodemailer = require("nodemailer");
+var uuid = require('node-uuid');
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "monit@brightdigit.com",
+        pass: "monit5ervices"
+    }
+});
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "monit@brightdigit.com", // sender address
+    to: "leogdion@brightdigit.com", // list of receivers
+    subject: "Confirmation Email", // Subject line
+    text: uuid.v4(), // plaintext body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+});
