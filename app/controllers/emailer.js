be72340185bb7a92a88d63deaf2b90ec37daae73
leogdion <leogdion@brightdigit.com>
2013-09-29 20:55:05 -0400

    working email test

--- app/controllers/emailer.js
+++ app/controllers/emailer.js
@@ -2,10 +2,23 @@ var nodemailer = require('nodemailer');
 var _ = require('underscore');
 
 module.exports = {
-	templates : {},
-	queue : function (template, data, callback) {
-		for (var name in template) {
-			
-		}
-	}
+  templates: {},
+  queue: function (template, data, callback) {
+    var mailOptions = {};
+
+    for (var name in template) {
+      mailOptions[name] = template[name](data);
+    }
+
+    var smtpTransport = nodemailer.createTransport("SMTP", {
+      service: "Gmail",
+      auth: {
+        user: "monit@brightdigit.com",
+        pass: "monit5ervices"
+      }
+    });
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, callback);
+  }
 };
--- test/controllers/emailer.js
+++ test/controllers/emailer.js
@@ -1,15 +1,26 @@
 var proxyquire = require('proxyquire');
+var _ = require('underscore');
 var emailer = proxyquire('../../app/controllers/emailer.js', {
   nodemailer : {
     createTransport : function (type, settings) {
-      return null;
+      return {
+      	sendMail : function (mailOptions, callback) {
+      		callback(undefined, mailOptions);
+      	}
+  	  }
     }
   }
 });
 
 exports.queue = {
   testValid: function (test) {
-    test.ok(false, "this assertion should fail");
-    test.done();
+  	var example = {
+  		body : _.template("hello: <%= name %>")
+  	};
+  	emailer.queue(example, {name : "test"}, function (error, response) {
+    	//test.ok(false, "this assertion should fail");
+    	test.strictEqual(response.body, "hello: test");
+    	test.done();
+  	});
   }
 };
