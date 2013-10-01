e47657672599c1bab7bbd173de8157aafcb0e293
Leo Dion <leogdion@brightdigit.com>
2013-10-01 14:15:30 -0400

    working on minify

--- minify.js
+++ minify.js
@@ -1,7 +1,27 @@
 var Git = require('git-wrapper'),
   path = require('path');
+  async = require('async'),
+  minify = require('minify'),
+  fs = require('fs');
 
 var git = new Git ();
+
+function minifyFile (fileName, cb) {
+  minify.optimize(fileName, {
+      callback: function(pData){
+          cb();
+          /*
+          fs.writeFile(fileName, pData, function(pError){
+            cb(pError);
+              //Util.log(pError || 'minify: ' + 'file ' + lOut +' writed');
+          });
+*/
+      }
+  });
+}
+
 git.exec('ls-files', function (error, message) {
-  console.log(message.split('\n'));
+  async.each(message.split('\n'), minifyFile, function (error) {
+    console.log(error);
+  });
 });
