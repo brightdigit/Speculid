ed0f9ddc4b3e23e0440092c35bd790f1b2ddb3d5
leogdion <leogdion@brightdigit.com>
2013-10-14 11:00:00 -0400

    added user controller, needs tests

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -66,45 +66,18 @@ module.exports = [{
           registration : findRegistration, 
           user : findUser
         }, function (err, results) {
-          console.log(results);
         if (results.registration !== undefined && !results.user) {
-          console.log(results);
           var user = User.create({ 
             name : request.body.name, 
             password : request.body.password, 
             emailAddress : request.body.emailAddress
-          }).setRegistration(results.registration).success(function (user) {
-            callback();
+          }).success(function (user) {
+            user.setRegistration(results.registration).success(function (user) {
+              callback();
+            });
           });
         }
         callback('Error');
       });
-      /*
-      var secret, key;
-      var emailAddress = request.body.emailAddress;
-
-      var data = {
-        emailAddress: request.body.emailAddress,
-        secret: new Buffer(uuid.parse(uuid.v4())),
-        key: new Buffer(uuid.parse(uuid.v4()))
-      };
-      var ar = registration.build(data);
-
-      ar.save().success(function(ar) {
-        emailer.queue('confirmation', {
-          emailAddress: data.emailAddress,
-          secret: data.secret.toString('base64')
-        }, function(error, response) {
-          callback(error ? 400 : undefined, error ? error : {
-            key: data.key.toString('base64')
-          });
-        });
-      }).error(function(error) {
-        if (error) {
-          callback(400, error);
-          return;
-        }
-      });
-  */
     }
   }];
--- app/models/user.js
+++ app/models/user.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  User.hasOne(Registration);
+  User.belongsTo(Registration);
 
   return User;
 };
