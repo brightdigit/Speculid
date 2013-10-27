9484c1a6a8dce50fa07c6a823b5b80c102e9fe0e
Leo Dion <leogdion@brightdigit.com>
2013-10-26 20:45:09 -0400

    start ios project

---
+++ client/ios/tgio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tgio
+//
+//  Created by Leo G Dion on 10/26/13.
+//  Copyright (c) 2013 Leo Dion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
