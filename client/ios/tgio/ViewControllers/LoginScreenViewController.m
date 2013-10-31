b950d6650236bbd5c6ace341142a4f3592241fd9
Leo Dion <leogdion@brightdigit.com>
2013-10-30 23:07:14 -0400

    added fonts, working on pixelate

---
+++ client/ios/tgio/ViewControllers/LoginScreenViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginScreenViewController.m
+//  tgio
+//
+//  Created by Leo G Dion on 10/30/13.
+//  Copyright (c) 2013 Leo Dion. All rights reserved.
+//
+
+#import "LoginScreenViewController.h"
+
+@interface LoginScreenViewController ()
+
+@end
+
+@implementation LoginScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- client/ios/tgio/main.m
+++ client/ios/tgio/main.m
@@ -5,7 +5,7 @@
 //  Created by Leo G Dion on 10/26/13.
 //  Copyright (c) 2013 Leo Dion. All rights reserved.
 //
-
+#import <Pixate/Pixate.h>
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
@@ -13,6 +13,8 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+        [Pixate licenseKey:@"J5R0V-BEGQB-53PNS-01QV9-0VP50-1R58F-UOTBE-SV1DG-10BA6-N0HH6-EFGGH-H9BKU-5L5VC-0H99D-0IMMP-5E" forUser:@"leogdion@brightdigit.com"];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+    
 }
