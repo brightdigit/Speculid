76a8077a88fa22b0256b9b1d0df7eda92135dcf8
Leo Dion <leogdion@brightdigit.com>
2013-11-04 16:59:17 -0500

    working on moving files to ui project

---
+++ client/ios/tgio-ui/tgio-ui/tgio_ui.m
@@ -0,0 +1,13 @@
+//
+//  tgio_ui.m
+//  tgio-ui
+//
+//  Created by Leo G Dion on 11/4/13.
+//  Copyright (c) 2013 Leo Dion. All rights reserved.
+//
+
+#import "tgio_ui.h"
+
+@implementation tgio_ui
+
+@end
