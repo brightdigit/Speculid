2918bd926e655e59e3285b2110e73f46c3b5c16f
Leo Dion <leogdion@brightdigit.com>
2014-02-10 20:27:11 -0500

    working on form validation

--- client/www/js/main.js
+++ client/www/js/main.js
@@ -1,3 +1,13 @@
 require(['jquery', 'app', 'bootstrap', 'jquery-validation', 'font!google,families:[Lato:300,Raleway]'], function($, App) {
+  /*
+  $.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            var re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your input."
+);
+  */
   App.initialize();
 });
--- client/www/js/views/loginregistration.js
+++ client/www/js/views/loginregistration.js
@@ -5,6 +5,7 @@ define([
   'templates',
   'models/session',
   'store',
+  'jquery-validation',
   // Using the Require.js text! plugin, we are loaded raw text
   // which will be used as our views primary template
 ], function($, Backbone, templates, SessionModel) {
@@ -17,7 +18,42 @@ define([
     },
     inputBlur: function() {},
     validate: function(callback) {
-      this.$('form').validate();
+      this.$('form').validate({
+        debug: true,
+        /*rules: {
+          name: {
+            required: true
+          },
+          password: {
+            required: true
+          },
+          confirmPassword: {
+            required: this.isRegistration
+          },
+          emailAddress: {
+            required: this.isRegistration
+          }
+        },*/
+        showErrors: function(errorMap, errorList) {
+          $.each(this.successList, function(index, value) {
+            return $(value).popover("hide");
+          });
+          return $.each(errorList, function(index, value) {
+            var _popover;
+            _popover = $(value.element).popover({
+              trigger: "manual",
+              placement: "top",
+              content: value.message,
+              template: "<div class=\"popover\"><div class=\"arrow\"></div><div class=\"popover-inner\"><div class=\"popover-content\"><p></p></div></div></div>"
+            });
+            // Bootstrap 3:
+            popover.data("bs.popover").options.content = value.message;
+            // Bootstrap 2.x:
+            //_popover.data("popover").options.content = value.message;
+            return $(value.element).popover("show");
+          });
+        }
+      });
     },
     register: function(evt) {
       if (this.isRegistration) {
