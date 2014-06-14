d0341e3e6ad1e888183460218b29ad4ded386c3c
Leo Dion <leogdion@brightdigit.com>
2014-06-13 20:10:58 -0400

    finished initial application

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var fs = require('fs');
+var app = require(__dirname + '/../app');
+chai.should();
+
+describe('it works', function () {
+  it('should do something', function () {
+    return app.listen.should.be.a('function');
+  });
+});
