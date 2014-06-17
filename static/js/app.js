6716765435f4c86011c4d1bed6e2af7f493ddfd8
Leo Dion <leogdion@brightdigit.com>
2014-06-16 22:09:40 -0400

    working on form modes

---
+++ static/js/app.js
@@ -0,0 +1,31 @@
+define(["backbone.marionette", "./router", 'views/home'], function (Marionette, Router, HomeView) {
+  var app = new Marionette.Application();
+
+  var Controller = Marionette.Controller.extend({
+
+    initialize: function (options) {
+      this.stuff = options.stuff;
+    },
+
+    index: function () {
+      app.mainRegion.show(new HomeView({
+
+      }));
+    }
+
+  });
+
+  app.addRegions({
+    headerRegion: "header",
+    mainRegion: "main"
+  });
+
+  app.addInitializer(function () {
+    new Router({
+      controller: new Controller()
+    });
+    Backbone.history.start();
+  });
+
+  return app;
+});
