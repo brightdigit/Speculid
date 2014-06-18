f702f2cee5e2a4686cfb2e26d0388306b0cf9be8
Leo Dion <leogdion@brightdigit.com>
2014-06-17 21:24:13 -0400

    working start page

--- static/js/views/home.js
+++ static/js/views/home.js
@@ -1,5 +1,32 @@
-define(['backbone.marionette', 'templates'], function (Marionette, templates) {
+define(['backbone.marionette', 'templates', 'backbone'], function (Marionette, templates, Backbone) {
   return Backbone.Marionette.ItemView.extend({
-    template: templates.home
+    template: templates.home,
+    events: {
+      "click button": "buttonClick",
+      "click #register.btn-primary": "register",
+      "click #signin.btn-primary": "signin"
+    },
+    register: function () {
+      Backbone.history.navigate('#confirmation', {
+        trigger: true
+      });
+    },
+    signin: function () {
+      Backbone.history.navigate('#home', {
+        trigger: true
+      });
+    },
+    buttonClick: function (event) {
+      var id = $(event.target).attr('id');
+      if (id === 'register') {
+        $('#signup').collapse('show');
+        $('button').removeClass('btn-primary');
+        $(event.target).addClass('btn-primary');
+      } else if (id === 'signin') {
+        $('#signup').collapse('hide');
+        $('button').removeClass('btn-primary');
+        $(event.target).addClass('btn-primary');
+      }
+    }
   });
 });
