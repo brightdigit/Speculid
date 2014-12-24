2eface45e084fb3e9b9795381fe04a6957908d72
Leo Dion <leogdion@brightdigit.com>
2014-12-24 14:22:00 -0500

    adding db

--- static/js/main.js
+++ static/js/main.js
@@ -4,5 +4,6 @@ var $ = require("browserify-zepto");
   http://ivomynttinen.com/blog/the-ios-7-design-cheat-sheet/
   http://iosdesign.ivomynttinen.com/
   https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/MobileHIG/IconMatrix.html#//apple_ref/doc/uid/TP40006556-CH27-SW1
+  http://ipad.about.com/od/Tablet_Computers_eReaders/a/The-iPad-Comparison-Chart.htm
 */
 console.log('Hello World');
