f079ae109733b3583ba8106626e98569aa3b1cbe
Leo Dion <leogdion@brightdigit.com>
2014-12-30 14:53:35 -0500

    working checkbox render

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 var $ = require("browserify-zepto"),
-  JST = require('JST').JST;
+  checkbox_option = require('../templates/checkbox_option.handlebars'),
+  data = require('data'),
+  forEach = require('./helpers/forEach');
 
 /*
   http://ivomynttinen.com/blog/the-ios-7-design-cheat-sheet/
@@ -8,4 +10,27 @@ var $ = require("browserify-zepto"),
   http://ipad.about.com/od/Tablet_Computers_eReaders/a/The-iPad-Comparison-Chart.htm
 */
 
-console.log(JST);
+function id (value) {
+  return value.replace(/\s/, "_");
+}
+
+function opt (field, value) {
+  return {
+    "field" : field,
+    "label" : value,
+    "id" : id(value)
+  }
+}
+
+function get (value) {
+  return (typeof(value) === 'string') ? value : (Object.keys(value)[0]);
+}
+
+forEach(data.os, function (key, value) {
+  $("#os").append(checkbox_option(opt("os", key)));
+
+});
+
+forEach(data.devices, function (key) {
+  $("#devices").append(checkbox_option(opt("dev",get(key))));
+});
