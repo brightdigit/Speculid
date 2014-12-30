0fe401a24eaec6f09ced5812388439035a7d9d9b
Leo Dion <leogdion@brightdigit.com>
2014-12-30 18:25:28 -0500

    basic working app

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,10 @@ gulp.task('serve', ['default'], function () {
   }
 });
 
+function get (value) {
+  return (typeof(value) === 'string') ? value : (Object.keys(value)[0]);
+}
+
 gulp.task('yaml', ['clean'],   function (cb) {
   if (!fs.existsSync('./.tmp')) {
     fs.mkdirSync('./.tmp');
@@ -45,7 +49,45 @@ gulp.task('yaml', ['clean'],   function (cb) {
     var result = {};
     result["os"] = data.os;
     result["devices"] = data.devices;
-    
+    var images = {};
+    var resolutions = {};
+
+    function getSize (value, device) {
+      if (Array.isArray(value)) {
+        return [value[0] * resolutions[device], value[value.length-1] * resolutions[device]];
+      } else {
+        return value * resolutions[device];
+      }
+      
+    }
+    for (var resolution in data.resolutions) {
+      for (var index = 0; index < data.resolutions[resolution].length; index++) {
+        resolutions[data.resolutions[resolution][index]] = resolution;
+      }
+    }
+
+    for (var name in data.images) {
+      var value = data.images[name];
+      var sizes;
+      if (typeof(value) === 'object' && !Array.isArray(value)) {
+        sizes = {};
+        for (var type in data.types) {
+          for (var index = 0; index < data.types[type].length; index++) {
+            sizes[data.types[type][index]] = getSize(value[type],data.types[type][index]); 
+          }
+        }
+      } else {
+
+        sizes = data.devices.reduce(function (memo, device) {
+          memo[get(device)] = getSize(value,get(device));
+          return memo;
+        }, {});
+      }
+      if (sizes) {
+        images[name] = sizes;
+      }
+    }
+    result["images"] = images;
     fs.writeFileSync('./.tmp/data.json', JSON.stringify(result));
     cb();
   });
