7bc856de45028485547694974bb6eea3e9b35346
Leo Dion <leogdion@brightdigit.com>
2016-09-23 07:51:39 -0400

    working on contents parsing

--- Speculid-App/AppDelegate.swift
+++ Speculid-App/AppDelegate.swift
@@ -17,7 +17,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // Insert code here to initialize your application
     #if DEBUG
     print("DEBUG")
-    let speculidURL = Bundle.main.url(forResource: "speculid", withExtension: "spcld")
+      if let speculidURL = Bundle.main.url(forResource: "speculid", withExtension: "spcld") {
+        if let data = try? Data(contentsOf: speculidURL) {
+          if let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+            if let dictionary = json as? [String : String] {
+              if let setRelativePath = dictionary["set"], let sourceRelativePath = dictionary["source"] {
+                
+                let contentsJSONURL = speculidURL.deletingLastPathComponent().appendingPathComponent(setRelativePath, isDirectory: true).appendingPathComponent("Contents.json")
+                let sourcePath = speculidURL.deletingLastPathComponent().appendingPathComponent(sourceRelativePath)
+                
+                if let contentsJSONData = try? Data(contentsOf: contentsJSONURL) {
+                  if let contentsJSON = try? JSONSerialization.jsonObject(with: contentsJSONData, options: []) as? [String : Any] {
+                    if let images = contentsJSON?["images"] as? [[String : String]] {
+                      print(images)
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
       
       
     #endif
