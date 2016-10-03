3903dfccc765eb5b576f72ba40de97c7603bcbea
Leo Dion <leogdion@brightdigit.com>
2016-10-03 11:49:12 -0400

    added examples and reorganized files

---
+++ examples/speculid-example-macos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  speculid-example-macos
+//
+//  Created by Leo Dion on 10/3/16.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
