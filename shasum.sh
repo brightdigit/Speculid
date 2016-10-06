058b17454f9a059fe7425979fbeb11185a48eb8d
Leo Dion <leogdion@brightdigit.com>
2016-10-06 09:52:33 -0400

    added parameters to sha script

--- shasum.sh
+++ shasum.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
-find "examples/Assets/." -name "*.spcld" -print0 |
+#!/bin/bash
+
+speculid_path=${1:-./build/Build/Products/Release/Speculid.app/Contents/MacOS/Speculid}
+assets_dir=${2:-examples/Assets/.}
+
+find $assets_dir -name "*.spcld" -print0 |
 while IFS= read -r -d $'\0' line; do
-"./build/Build/Products/Release/Speculid.app/Contents/MacOS/Speculid" "$line" >/dev/null
+ $speculid_path "$line" >/dev/null
 done
 wait
-find "examples/Assets/." -type f \( -iname \*.icns -o -iname \*.png \) -print0 | sort -z | xargs -0 shasum -a 512 | shasum -a 512
+find $assets_dir -type f \( -iname \*.icns -o -iname \*.png \) -print0 | sort -z | xargs -0 shasum -a 512 | shasum -a 512
