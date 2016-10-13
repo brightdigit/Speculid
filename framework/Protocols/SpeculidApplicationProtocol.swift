3cd8b2a87ddf6e6aea270f54e6416f40cbfe4aee
Leo Dion <leogdion@brightdigit.com>
2016-10-13 19:32:16 -0400

    fixing issues with GA

--- framework/Protocols/SpeculidApplicationProtocol.swift
+++ framework/Protocols/SpeculidApplicationProtocol.swift
@@ -10,7 +10,6 @@ import Foundation
 import SwiftVer
 
 public protocol SpeculidApplicationProtocol {
-  func begin (withArguments arguments: SpeculidArgumentsProtocol, _ callback: (SpeculidApplicationProtocol) -> Void)
   func document (url: URL) -> SpeculidDocumentProtocol?
   var builder : SpeculidBuilderProtocol { get }
   var version : Version { get }
