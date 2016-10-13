355869184115cd19ab1c625ac13d40d49ef2341b
Leo Dion <leogdion@brightdigit.com>
2016-10-13 17:32:41 -0400

    migrating to google analytics

--- framework/Controllers/Speculid.swift
+++ framework/Controllers/Speculid.swift
@@ -16,4 +16,5 @@ public struct Speculid {
   
   public static let vcs = VersionControlInfo(TYPE: VCS_TYPE, BASENAME: VCS_BASENAME, UUID: VCS_UUID, NUM: VCS_NUM, DATE: VCS_DATE, BRANCH: VCS_BRANCH, TAG: VCS_TAG, TICK: VCS_TICK, EXTRA: VCS_EXTRA, FULL_HASH: VCS_FULL_HASH, SHORT_HASH: VCS_SHORT_HASH, WC_MODIFIED: VCS_WC_MODIFIED)
   public static let version = Version(bundle: Bundle(for: _VersionHandler.self), versionControl: vcs)!
+  public static let application : SpeculidApplicationProtocol! = nil
 }
