4c26e9fc1d87fbc707bec46fab9ba1b8dfe5eca9
Leo Dion <leogdion@brightdigit.com>
2016-10-17 14:37:48 -0400

    refactored application code

--- shasum.sh
+++ shasum.sh
@@ -3,9 +3,9 @@
 speculid_path=${1:-./build/Build/Products/Release/Speculid.app/Contents/MacOS/Speculid}
 assets_dir=${2:-examples/Assets/.}
 
-find $assets_dir -name "*.spcld" -print0 |
+find $assets_dir -name "*.speculid" -print0 |
 while IFS= read -r -d $'\0' line; do
  $speculid_path "$line" >/dev/null
 done
 wait
-(cd $assets_dir && find . -type f \( -iname \*.icns -o -iname \*.png \) -print0 | sort -z | xargs -0 shasum -a 512 | shasum -a 512)
+(cd $assets_dir && find . -type f \( -iname \*.icns -o -iname \*.png -o -iname \*.pdf \) -print0 | sort -z | xargs -0 shasum -a 512 | shasum -a 512)
