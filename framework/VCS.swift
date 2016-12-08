3ab2e0502228c2935d45ecf130ea92d7373c21f0
Leo Dion <leogdion@brightdigit.com>
2016-12-07 20:23:17 -0500

    added fix for scaling, updated shasum

--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 442
-let VCS_DATE			= "2016-12-02T02:29:30Z"
-let VCS_BRANCH:	String	= "hotfix/fabric"
-let VCS_TAG:	String?	= "1.0.1"
-let VCS_TICK:	Int?	= 4
-let VCS_EXTRA:	String?	= "53"
+let VCS_NUM:	Int		= 444
+let VCS_DATE			= "2016-12-02T14:58:22Z"
+let VCS_BRANCH:	String	= "develop"
+let VCS_TAG:	String?	= "1.0.0"
+let VCS_TICK:	Int?	= 13
+let VCS_EXTRA:	String?	= "57"
 
-let VCS_ACTION_STAMP:	String?	= "2016-12-02T02:29:30Z!leogdion@brightdigit.com"
-let VCS_FULL_HASH:		String	= "5ff757f67362ad7a5f97e2de739cf1b55041790d"
-let VCS_SHORT_HASH:		String	= "5ff757f"
+let VCS_ACTION_STAMP:	String?	= "2016-12-02T14:58:22Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "4c4d99a47a8cfd9607ae5ca8052935a45f977da3"
+let VCS_SHORT_HASH:		String	= "4c4d99a"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
