abaab0b775edd1bdf04570def85dbe96758424ed
Leo Dion <leogdion@brightdigit.com>
2016-12-09 09:32:01 -0500

    fixed version number

--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 444
-let VCS_DATE			= "2016-12-02T14:58:22Z"
-let VCS_BRANCH:	String	= "develop"
+let VCS_NUM:	Int		= 446
+let VCS_DATE			= "2016-12-09T13:58:01Z"
+let VCS_BRANCH:	String	= "release/1.0.1"
 let VCS_TAG:	String?	= "1.0.0"
-let VCS_TICK:	Int?	= 13
-let VCS_EXTRA:	String?	= "57"
+let VCS_TICK:	Int?	= 15
+let VCS_EXTRA:	String?	= "1"
 
-let VCS_ACTION_STAMP:	String?	= "2016-12-02T14:58:22Z!leogdion@brightdigit.com"
-let VCS_FULL_HASH:		String	= "4c4d99a47a8cfd9607ae5ca8052935a45f977da3"
-let VCS_SHORT_HASH:		String	= "4c4d99a"
+let VCS_ACTION_STAMP:	String?	= "2016-12-09T13:58:01Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "f5aa5c3d932917928e2310712a037123f2d4e14f"
+let VCS_SHORT_HASH:		String	= "f5aa5c3"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
