977353501bce36ec0ca2bfcbddc7224f4e087d46
Leo Dion <leogdion@brightdigit.com>
2016-12-21 16:52:27 -0500

    fixed shasum

--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 455
-let VCS_DATE			= "2016-12-21T13:43:07Z"
+let VCS_NUM:	Int		= 457
+let VCS_DATE			= "2016-12-21T17:39:11Z"
 let VCS_BRANCH:	String	= "feature/remove-alpha-channel"
 let VCS_TAG:	String?	= "1.0.1"
-let VCS_TICK:	Int?	= 5
-let VCS_EXTRA:	String?	= "63"
+let VCS_TICK:	Int?	= 7
+let VCS_EXTRA:	String?	= "71"
 
-let VCS_ACTION_STAMP:	String?	= "2016-12-21T13:43:07Z!leogdion@brightdigit.com"
-let VCS_FULL_HASH:		String	= "73727fa5e85a42d94b4de3fd18744fecf6510722"
-let VCS_SHORT_HASH:		String	= "73727fa"
+let VCS_ACTION_STAMP:	String?	= "2016-12-21T17:39:11Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "0405254078b396d1afa1a748bac64a3b9e088efe"
+let VCS_SHORT_HASH:		String	= "0405254"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
