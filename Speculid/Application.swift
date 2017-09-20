ace1fe923488315cc2f870ff8d5ddc36a3334505
Leo Dion <leogdion@brightdigit.com>
2017-09-19 20:17:55 -0400

    working application

--- Speculid/Application.swift
+++ Speculid/Application.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-public struct Application {
-  public static func helloWorld () {
-    print("hello World")
+open class Application : NSApplication {
+  var statusItem: NSStatusItem!
+  
+  open override func finishLaunching() {
+    super.finishLaunching()
+    
+    let menu = NSMenu(title: "Speculid")
+    let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    item.title = "Speculid"
+    item.image = #imageLiteral(resourceName: "TrayIcon")
+    item.image?.isTemplate = true
+    item.menu = menu
+    self.statusItem = item
   }
 }
