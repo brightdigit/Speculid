439cc45ea29515da8d690ca3280e3c460f381ab7
Leo Dion <leogdion@brightdigit.com>
2017-09-21 11:18:17 -0400

    Initial Commit

---
+++ obj-librsvg/obj_librsvg.h
@@ -0,0 +1,19 @@
+//
+//  obj_librsvg.h
+//  obj-librsvg
+//
+//  Created by Leo Dion on 9/21/17.
+//  Copyright © 2017 Leo Dion. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for obj_librsvg.
+FOUNDATION_EXPORT double obj_librsvgVersionNumber;
+
+//! Project version string for obj_librsvg.
+FOUNDATION_EXPORT const unsigned char obj_librsvgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <obj_librsvg/PublicHeader.h>
+
+
