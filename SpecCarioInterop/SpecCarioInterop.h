934012f47dc67cf9f196ea94dc3b8145f85dd9cb
Leo Dion <leogdion@brightdigit.com>
2017-09-28 10:22:52 -0400

    moved objective-c code over

---
+++ SpecCarioInterop/SpecCarioInterop.h
@@ -0,0 +1,12 @@
+//
+//  SpecCarioInterop.h
+//  SpecCarioInterop
+//
+//  Created by Leo Dion on 9/28/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpecCarioInterop : NSObject
+
+@end
--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 468
-let VCS_DATE			= "2016-12-30T19:31:12Z"
-let VCS_BRANCH:	String	= "master"
-let VCS_TAG:	String?	= "1.0.1"
-let VCS_TICK:	Int?	= 18
-let VCS_EXTRA:	String?	= "57"
+let VCS_NUM:	Int		= 478
+let VCS_DATE			= "2017-09-28T14:01:33Z"
+let VCS_BRANCH:	String	= "feature/remove-inkscape-imagick"
+let VCS_TAG:	String?	= "1.0.2"
+let VCS_TICK:	Int?	= 9
+let VCS_EXTRA:	String?	= "2"
 
-let VCS_ACTION_STAMP:	String?	= "2016-12-30T19:31:12Z!noreply@github.com"
-let VCS_FULL_HASH:		String	= "3f0a0097d2f25136ff8c5b2ab0d52f26449ffa86"
-let VCS_SHORT_HASH:		String	= "3f0a009"
+let VCS_ACTION_STAMP:	String?	= "2017-09-28T14:01:33Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "a8e1f462246343eedcf4cee5a47023252bbbf6d1"
+let VCS_SHORT_HASH:		String	= "a8e1f46"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
