a198ec5383bb5c270a9b9e87ec7b73466c5196f4
Leo Dion <leogdion@brightdigit.com>
2017-09-29 21:10:34 -0400

    fixing code_signing

--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 491
-let VCS_DATE			= "2017-09-29T12:40:03Z"
+let VCS_NUM:	Int		= 493
+let VCS_DATE			= "2017-09-29T14:07:39Z"
 let VCS_BRANCH:	String	= "feature/swift4"
 let VCS_TAG:	String?	= "1.0.2"
-let VCS_TICK:	Int?	= 22
+let VCS_TICK:	Int?	= 24
 let VCS_EXTRA:	String?	= "1"
 
-let VCS_ACTION_STAMP:	String?	= "2017-09-29T12:40:03Z!leogdion@brightdigit.com"
-let VCS_FULL_HASH:		String	= "95df67b4bee457cfd46dfb55528145ddf00edf67"
-let VCS_SHORT_HASH:		String	= "95df67b"
+let VCS_ACTION_STAMP:	String?	= "2017-09-29T14:07:39Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "3627cc61cc2fd0a22e5ceec4a93ce82e5623bb0a"
+let VCS_SHORT_HASH:		String	= "3627cc6"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
