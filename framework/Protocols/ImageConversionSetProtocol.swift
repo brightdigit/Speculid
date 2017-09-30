075047dbbbdfd47d07727bd3b44dfc64a018066f
Leo Dion <leogdion@brightdigit.com>
2017-09-30 07:05:26 -0400

    working on framework/Controllers/CollectionConversionBuilder.swift

---
+++ framework/Protocols/ImageConversionSetProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ImageConversionSetProtocol.swift
+//  Speculid
+//
+//  Created by Leo Dion on 9/30/17.
+//
+
+import Foundation
+
+public protocol ImageConversionSetProtocol {
+  
+}
--- framework/VCS.swift
+++ framework/VCS.swift
@@ -3,16 +3,16 @@
 let VCS_TYPE			= "git"
 let VCS_BASENAME		= "speculid"
 let VCS_UUID:	String?	= "b6fd659a7d2088d5420f962a307b1f7f7e5d0604"
-let VCS_NUM:	Int		= 493
-let VCS_DATE			= "2017-09-29T14:07:39Z"
-let VCS_BRANCH:	String	= "feature/swift4"
+let VCS_NUM:	Int		= 494
+let VCS_DATE			= "2017-09-30T01:10:34Z"
+let VCS_BRANCH:	String	= "feature/remove-inkscape-imagick"
 let VCS_TAG:	String?	= "1.0.2"
-let VCS_TICK:	Int?	= 24
-let VCS_EXTRA:	String?	= "1"
+let VCS_TICK:	Int?	= 25
+let VCS_EXTRA:	String?	= "10"
 
-let VCS_ACTION_STAMP:	String?	= "2017-09-29T14:07:39Z!leogdion@brightdigit.com"
-let VCS_FULL_HASH:		String	= "3627cc61cc2fd0a22e5ceec4a93ce82e5623bb0a"
-let VCS_SHORT_HASH:		String	= "3627cc6"
+let VCS_ACTION_STAMP:	String?	= "2017-09-30T01:10:34Z!leogdion@brightdigit.com"
+let VCS_FULL_HASH:		String	= "70d2d36b994c11e85f5fbbb1432a918205a39e27"
+let VCS_SHORT_HASH:		String	= "70d2d36"
 
 let VCS_WC_MODIFIED:	Bool	= true
 
