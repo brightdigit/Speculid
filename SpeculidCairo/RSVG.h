e219a707a57029846ad37e962797e03870c23278
Leo Dion <leogdion@brightdigit.com>
2017-10-02 16:33:40 -0400

    working on interop

--- SpeculidCairo/RSVG.h
+++ SpeculidCairo/RSVG.h
@@ -7,10 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ImageHandle.h"
+
+typedef NS_ENUM(NSUInteger, Dimension) {
+  kWidth,
+  kHeight
+};
+
+struct GeometryDimension {
+  double value;
+  Dimension dimension;
+};
 
 @interface RSVG : NSObject
 + (void)createPNGFromSVG;
 + (void)createPDFFromSVG;
 + (void)createPNGFromPNG;
-+ (void)exportImageAtURL:(NSURL*) sourceURL toURL:(NSURL*) destinationURL error: (NSError**) errorptr;
++  (void)exportImage:(id<ImageHandle>) sourceHandle toURL:(NSURL*) destinationURL withDimensions: (struct GeometryDimension) dimensions shouldRemoveAlphaChannel: (BOOL) removeAlphaChannel setBackgroundColor: (CGColorRef) backgroundColor error: (NSError**) errorptr;
 @end
--- Speculid_Mac_App/Application.swift
+++ Speculid_Mac_App/Application.swift
@@ -9,5 +9,9 @@ import Cocoa
 import Speculid
 
 open class Application: Speculid.Application {
-  
+  public func test () {
+    let url = Bundle.main.url(forResource: "layers", withExtension: "svg")
+    let handle = try! ImageHandleBuilder.shared().imageHandle(from: url)
+    
+  }
 }
