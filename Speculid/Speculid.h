142bf2bfb29bc2e0e1c6698f64da90f69cf2149b
Leo Dion <leogdion@brightdigit.com>
2017-10-05 13:07:07 -0400

    working example

---
+++ Speculid/Speculid.h
@@ -0,0 +1,18 @@
+//
+//  Speculid.h
+//  Speculid
+//
+//  Created by Leo Dion on 10/5/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Speculid.
+FOUNDATION_EXPORT double SpeculidVersionNumber;
+
+//! Project version string for Speculid.
+FOUNDATION_EXPORT const unsigned char SpeculidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Speculid/PublicHeader.h>
+
+
