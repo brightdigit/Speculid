0991e01e760f3cd5e7177aabbbfbcb84acd9a3c8
Leo Dion <leogdion@brightdigit.com>
2017-10-20 14:35:56 -0400

    refactoring command line parsing

---
+++ frameworks/speculid/Models/SpeculidCommandArgumentSet.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public enum SpeculidCommandArgumentSet: Equatable {
+  public static func ==(lhs: SpeculidCommandArgumentSet, rhs: SpeculidCommandArgumentSet) -> Bool {
+    switch (lhs, rhs) {
+    case (.help, .help): return true
+    case (.unknown, .unknown): return true
+    case (.version, .version): return true
+    case let (.file(l), .file(r)): return l == r
+    default: return false
+    }
+  }
+
+  case help
+  case unknown([String])
+  case version
+  case file(URL)
+}
--- frameworks/speculid/Protocols/ApplicationProtocol.swift
+++ frameworks/speculid/Protocols/ApplicationProtocol.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public protocol ApplicationProtocol {
   func document(url: URL) -> SpeculidDocumentProtocol?
-  var builder: SpeculidBuilderProtocol { get }
-  var service: ServiceProtocol { get }
-  var regularExpressions: RegularExpressionSetProtocol { get }
-  var tracker: RegularExpressionSetProtocol { get }
+  var builder: SpeculidBuilderProtocol! { get }
+  var service: ServiceProtocol! { get }
+  var regularExpressions: RegularExpressionSetProtocol! { get }
+  var tracker: AnalyticsTrackerProtocol! { get }
   // var version : Version { get }
 }
