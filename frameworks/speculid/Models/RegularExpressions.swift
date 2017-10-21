c9307a03b880426ca650b1478ef3abd1237e9e17
Leo Dion <leogdion@brightdigit.com>
2017-10-20 22:53:42 -0400

    completing merge with develop

--- frameworks/speculid/Models/RegularExpressions.swift
+++ frameworks/speculid/Models/RegularExpressions.swift
@@ -1,25 +1,13 @@
-//
-//  RegularExpressions.swift
-//  Speculid-Mac-App
-//
-//  Created by Leo Dion on 10/14/17.
-//  Copyright © 2017 Bright Digit, LLC. All rights reserved.
-//
-
 import Foundation
 
 public enum RegularExpressionKey {
-  
 }
 
 public typealias RegularExpressionParameters = (String, NSRegularExpression.Options)
 
 public struct RegularExpressions {
-  public let dictionary : [RegularExpressionKey : NSRegularExpression]
+  public let dictionary: [RegularExpressionKey: NSRegularExpression]
 
-  public init (dictionary : [RegularExpressionKey : RegularExpressionParameters]) {
-    
+  public init(dictionary _: [RegularExpressionKey: RegularExpressionParameters]) {
   }
 }
-
-
