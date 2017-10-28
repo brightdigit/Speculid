207c574e60b1800ca291826f65bb66bb16afeabe
Leo Dion <leogdion@brightdigit.com>
2017-10-27 20:48:23 -0400

    added developerid certification

--- decrypt-certs.sh
+++ decrypt-certs.sh
@@ -8,4 +8,5 @@
 mkdir -p tmp/certs
 openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/mac_app-cert.cer.enc -d -a -out tmp/certs/mac_app-cert.cer
 openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/mac_development-cert.cer.enc -d -a -out tmp/certs/mac_development-cert.cer
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/mac_development-key.p12.enc -d -a -out tmp/certs/mac_development-key.p12
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/developerid-cert.cer.enc -d -a -out tmp/certs/developerid-cert.cer
+openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/mac_development-key.p12.enc -d -a -out tmp/certs/develop-key.p12
