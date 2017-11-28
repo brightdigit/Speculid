4221e81874cfd0351c3bbd260be1244f3e3af845
Leo Dion <leogdion@brightdigit.com>
2017-11-27 20:42:38 -0500

    fixed problem with brew remove

--- build.sh
+++ build.sh
@@ -13,5 +13,5 @@ xcodebuild -workspace speculid.xcworkspace -scheme "CairoSVG-Flags" -derivedData
 xcodebuild archive -workspace speculid.xcworkspace -scheme "Speculid-Mac-App" -configuration Debug -derivedDataPath ./build -archivePath ./build/Products/Speculid.xcarchive
 xcodebuild -exportArchive -archivePath ./build/Products/Speculid.xcarchive -exportOptionsPlist exportOptions.plist -exportPath ./build/Products/App
 ditto -c -k --sequesterRsrc --keepParent ./build/Products/App/Speculid.app build/Speculid.zip
-brew remove --ignore-dependencies librsvg cairo
+brew remove --ignore-dependencies --force librsvg cairo
 open Build/Products/App/Speculid.app
