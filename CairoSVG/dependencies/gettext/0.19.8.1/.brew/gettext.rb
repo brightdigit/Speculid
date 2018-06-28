ef6065835ca0370bab9ba17226aa7190432ddae8
Leo Dion <leogdion@brightdigit.com>
2018-06-28 12:09:02 -0400

    adding dependencies directly from homebrew

---
+++ CairoSVG/dependencies/gettext/0.19.8.1/.brew/gettext.rb
@@ -0,0 +1,39 @@
+class Gettext < Formula
+  desc "GNU internationalization (i18n) and localization (l10n) library"
+  homepage "https://www.gnu.org/software/gettext/"
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.19.8.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.19.8.1.tar.xz"
+  sha256 "105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4"
+
+  keg_only :shadowed_by_osx,
+    "macOS provides the BSD gettext library & some software gets confused if both are in the library path"
+
+  # https://savannah.gnu.org/bugs/index.php?46844
+  depends_on "libxml2" if MacOS.version <= :mountain_lion
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-included-gettext",
+                          "--with-included-glib",
+                          "--with-included-libcroco",
+                          "--with-included-libunistring",
+                          "--with-emacs",
+                          "--with-lispdir=#{elisp}",
+                          "--disable-java",
+                          "--disable-csharp",
+                          # Don't use VCS systems to create these archives
+                          "--without-git",
+                          "--without-cvs",
+                          "--without-xz"
+    system "make"
+    ENV.deparallelize # install doesn't support multiple make jobs
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gettext", "test"
+  end
+end
