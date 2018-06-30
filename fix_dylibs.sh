51d9020b1d6bc6450f6245247a141a805757410e
Leo Dion <leogdion@brightdigit.com>
2018-06-30 12:36:07 -0400

    fixing dylibs

--- fix_dylibs.sh
+++ fix_dylibs.sh
@@ -6,32 +6,32 @@
 ##  Created by Leo Dion on 11/27/17.
 ##  Copyright © 2017 Bright Digit, LLC. All rights reserved.
 #
-#for (( c=1; c<=10; c++ ))
-#do
-#  echo "Iteration: $c"
-#  DEPCOUNT=0
-#  DYLIBS=`ls "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH"`
-#  for dylib in $DYLIBS; do
-#otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
-#    DEPS=`otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib" | grep "/opt\|Cellar" | awk -F' ' '{ print $1 }'`
-#
+for (( c=1; c<=10; c++ ))
+do
+  echo "Iteration: $c"
+  DEPCOUNT=0
+  DYLIBS=`ls "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH"`
+  for dylib in $DYLIBS; do
+otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
+    DEPS=`otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib" | grep "/opt\|Cellar" | awk -F' ' '{ print $1 }'`
+
 #    for dependency in $DEPS; do
-#    echo "Installing $dependency"
-#    install -m 755 $dependency "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH"
-#
+#   echo "Installing $dependency"
+#install -m 755 $dependency "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH"
+
 #    done
-#    for dependency in $DEPS; do
-#      echo "Fixing $dependency"
-#      install_name_tool -id @rpath/`basename $dependency` "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
-#      install_name_tool -change $dependency @rpath/`basename $dependency` "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
-#      DEPCOUNT=$((DEPCOUNT+1))
-#    done
-#  done
-#  echo "Dependency Count: $DEPCOUNT"
-#  if [ $DEPCOUNT -eq 0 ]; then exit; fi
-#done
-#
-#
+    for dependency in $DEPS; do
+      echo "Fixing $dependency"
+      install_name_tool -id @rpath/`basename $dependency` "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
+      install_name_tool -change $dependency @rpath/`basename $dependency` "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$dylib"
+      DEPCOUNT=$((DEPCOUNT+1))
+    done
+  done
+  echo "Dependency Count: $DEPCOUNT"
+  if [ $DEPCOUNT -eq 0 ]; then exit; fi
+done
+
+
 DEPS=`otool -L "$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH" | grep "/opt\|Cellar" | awk -F' ' '{ print $1 }'`
 for dependency in $DEPS; do
   echo "Installing $dependency"
@@ -40,11 +40,11 @@ for dependency in $DEPS; do
   install_name_tool -change $dependency @rpath/`basename $dependency` "$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH"
   otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/`basename $dependency`"
   BASENAME=`basename $dependency`
-  SUBDEPS=`otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$BASENAME" | grep "/opt\|Cellar" | awk -F' ' '{ print $1 }'`
-  for subdependency in $SUBDEPS; do
-    echo "Installing `basename $subdependency` for $BASENAME"
-    install_name_tool -id @rpath/`basename $subdependency` "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/`basename $subdependency`"
-    install_name_tool -change $dependency @rpath/`basename $subdependency` "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/$BASENAME"
-  done
+#  SUBDEPS=`otool -L "$TARGET_BUILD_DIR/$FRAMEWORKS_FOLDER_PATH/$BASENAME" | grep "/opt\|Cellar" | awk -F' ' '{ print $1 }'`
+#  for subdependency in $SUBDEPS; do
+#    echo "Installing `basename $subdependency` for $BASENAME"
+#    install_name_tool -id @rpath/`basename $subdependency` "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/`basename $subdependency`"
+#    install_name_tool -change $dependency @rpath/`basename $subdependency` "$BUILT_PRODUCTS_DIR/$FRAMEWORKS_FOLDER_PATH/$BASENAME"
+#  done
 
 done
