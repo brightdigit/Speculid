29f2e2d8c1cb772d4b5f764948c090155c707cea
Leo Dion <leogdion@brightdigit.com>
2018-07-11 16:44:13 -0400

    adding developer_id cert

--- decrypt-certs.sh
+++ decrypt-certs.sh
@@ -11,5 +11,7 @@ mkdir -p tmp/certs
 #openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/mac_development-cert.cer.enc -d -a -out tmp/certs/mac_development-cert.cer
 openssl aes-256-cbc -k "$CERTIFICATE_PASSWORD" -in certs/mac_developer.p12.enc -d -a -out tmp/certs/mac_developer.p12
 openssl aes-256-cbc -k "$CERTIFICATE_PASSWORD" -in certs/mac_developer.cer.enc -d -a -out tmp/certs/mac_developer.cer
+openssl aes-256-cbc -k "$CERTIFICATE_PASSWORD" -in certs/developer_id.p12.enc -d -a -out tmp/certs/developer_id.p12
+openssl aes-256-cbc -k "$CERTIFICATE_PASSWORD" -in certs/developer_id.cer.enc -d -a -out tmp/certs/developer_id.cer
 #openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/3rd_party_mac_development-key.p12.enc -d -a -out tmp/certs/3rd_party_mac_development-key.p12
 #openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in certs/3rd_party_mac_development-cert.cer.enc -d -a -out tmp/certs/3rd_party_mac_development-cert.cer
