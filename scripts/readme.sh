5ad710980bc18be0d89ffa107cd81c1aed3ce0cf
Leo Dion <leogdion@brightdigit.com>
2018-08-05 15:40:28 -0400

    fixing image in README script

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -5,9 +5,8 @@ DESTINATION=README.md
 
 cat $SOURCE | \
 	perl -pe 's/\{\:height="[^"]+"\}//g' | \
-	perl -pe 's/\]\(\//\]\(\/gh-pages\//g' | \
-	#perl -pe 's/\\{\:class="html-only"\}//g' | \
-	#![Multiple Images](/images/mechanic.svg){:height="100px"}{:class="html-only"}
+	perl -pe 's/\]\(\/([^\/])/\]\(https\:\/\/raw.githubusercontent.com\/brightdigit\/Speculid\/gh-pages\/$1/g' | \
+	perl -pe 's/src="\//src="https\:\/\/raw.githubusercontent.com\/brightdigit\/Speculid\/gh-pages\//g' | \
 	perl -pe 's/\!.+\{\:class="html-only"\}//g' | \
 	perl -0pe 's/\<\!-- HTML-ONLY BEGIN --\>.*\<\!-- HTML-ONLY END --\>//gms' | \
 	cat > $DESTINATION
