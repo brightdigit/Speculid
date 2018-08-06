f76ab0521510327f652e0a34b4f273de7e768f7a
Leo Dion <leogdion@brightdigit.com>
2018-08-05 22:43:23 -0400

    added table of contents

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -9,4 +9,8 @@ cat $SOURCE | \
 	perl -pe 's/src="\//src="https\:\/\/cdn.rawgit.com\/brightdigit\/Speculid\/gh-pages\//g' | \
 	perl -pe 's/\!.+\{\:class="html-only"\}//g' | \
 	perl -0pe 's/\<\!-- HTML-ONLY BEGIN --\>.*\<\!-- HTML-ONLY END --\>//gms' | \
+	perl -0pe 's/\* TOC.*{:toc}/<!--ts-->\n<!--te-->/gms' | \
+
 	cat > $DESTINATION
+
+"${BASH_SOURCE%/*}/github-markdown-toc/gh-md-toc"  --insert $DESTINATION
