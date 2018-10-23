036d870f51797e96bbc37f9e5e822013722606a7
Leo Dion <leogdion@brightdigit.com>
2018-10-23 16:40:29 -0400

    working on installer service

--- frameworks/speculid/Protocols/RemoteObjectInterfaceProviderProtocol.swift
+++ frameworks/speculid/Protocols/RemoteObjectInterfaceProviderProtocol.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-public protocol RemoteObjectInterfaceProviderProtocol {
+public protocol RemoteObjectInterfaceProviderProtocol where ProxyType: Protocol {
+  associatedtype ProxyType
+  func remoteObjectProxyWith(serviceName: String, andHandler handler: (Result<ProxyType>) -> Void)
+}
+
+public protocol ServiceObjectInterfaceProvider {
   func remoteObjectProxyWithHandler(_ handler: (Result<ServiceProtocol>) -> Void)
 }
