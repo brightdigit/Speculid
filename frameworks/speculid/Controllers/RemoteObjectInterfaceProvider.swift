65b3dcc96cbc97b10c952e4996ef8f97e7970436
Leo Dion <leogdion@brightdigit.com>
2018-10-24 09:48:25 -0400

    fixing service

--- frameworks/speculid/Controllers/RemoteObjectInterfaceProvider.swift
+++ frameworks/speculid/Controllers/RemoteObjectInterfaceProvider.swift
@@ -29,3 +29,32 @@ public struct RemoteObjectInterfaceProvider: RemoteObjectInterfaceProviderProtoc
     handler(result)
   }
 }
+
+public struct InstallerObjectInterfaceProvider: InstallerObjectInterfaceProviderProtocol {
+  public func remoteObjectProxyWithHandler(_ handler: (Result<InstallerProtocol>) -> Void) {
+    let interface = NSXPCInterface(with: InstallerProtocol.self)
+    let connection =
+      NSXPCConnection(machServiceName: "com.brightdigit.Speculid-Mac-Installer", options: .privileged)
+
+    connection.remoteObjectInterface = interface
+    connection.resume()
+
+    var error: Error?
+
+    let proxy = connection.remoteObjectProxyWithErrorHandler { handlerError in
+      error = handlerError
+    }
+
+    let result: Result<InstallerProtocol>
+
+    if let error = error {
+      result = .error(error)
+    } else if let service = proxy as? InstallerProtocol {
+      result = .success(service)
+    } else {
+      result = .error(NoServiceReturnedError())
+    }
+
+    handler(result)
+  }
+}
--- frameworks/speculid/Protocols/RemoteObjectInterfaceProviderProtocol.swift
+++ frameworks/speculid/Protocols/RemoteObjectInterfaceProviderProtocol.swift
@@ -3,3 +3,7 @@ import Foundation
 public protocol RemoteObjectInterfaceProviderProtocol {
   func remoteObjectProxyWithHandler(_ handler: (Result<ServiceProtocol>) -> Void)
 }
+
+public protocol InstallerObjectInterfaceProviderProtocol {
+  func remoteObjectProxyWithHandler(_ handler: (Result<InstallerProtocol>) -> Void)
+}
