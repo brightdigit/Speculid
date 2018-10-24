e7ac323a6da53a14fd95c6686c09b44a46b8ded7
Leo Dion <leogdion@brightdigit.com>
2018-10-24 08:52:03 -0400

    cleaned up fix script

--- scripts/fix_dylibs.sh
+++ scripts/fix_dylibs.sh
@@ -9,7 +9,6 @@ done
 FRAMEWORKS_FOLDER_PATH="`dirname $1`/Frameworks/"
 deps=`ls "$FRAMEWORKS_FOLDER_PATH" | awk -F' ' '{ print $1 }'`
 for lib in $deps; do
-  echo "Fixing $dependency"
   install_name_tool -id @rpath/`basename $lib` "`dirname $1`/Frameworks/`basename $lib`"
   install_name_tool -change $lib @rpath/`basename $lib` "$1"
   dylib="`dirname $1`/Frameworks/`basename $lib`"
