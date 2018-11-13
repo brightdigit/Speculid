7c06ad66e90936d455ef1e874b3654d8aa74d825
Leo Dion <leogdion@brightdigit.com>
2018-11-13 11:49:52 -0500

    adding speculid cask for issue #11

---
+++ packages/homebrew/speculid.rb
@@ -0,0 +1,10 @@
+cask 'speculid' do
+  version '2.0.0-alpha.3'
+  sha256 'a42856845a422893e0493638c8ae8f6ea282b74a80075474f527836a4d5ae605'
+
+  url 'https://github.com/brightdigit/Speculid/releases/download/v2.0.0-alpha.3/Speculid.zip'
+  name 'Speculid'
+  homepage 'https://speculid.com'
+
+  app 'Speculid.App'
+end
