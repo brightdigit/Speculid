d51660deb4360542cfed0072f1dc8d6da0045516
Leo Dion <leogdion@brightdigit.com>
2020-01-17 19:27:42 -0500

    working on building new icons

--- TestSetup/main.swift
+++ TestSetup/main.swift
@@ -18,7 +18,9 @@ let folderContentsJson = """
 """
 
 
-let appIconUrl = URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/AppIcon.speculid")
+let appIconSpeculidUrl = URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/AppIcon.speculid")
+
+let imageSetSpeculidUrl = URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/Image Set.speculid")
 guard let filePath = CommandLine.arguments.last.flatMap({
   URL(fileURLWithPath: $0)
 }) else {
@@ -33,10 +35,10 @@ let assetsParentUrl = URL(fileURLWithPath: "/Users/leo/Documents/Projects/Specul
 
 let speculidIconsUrl = assetsParentUrl.appendingPathComponent("icons", isDirectory: true)
 let assetsUrl = assetsParentUrl.appendingPathComponent("Assets.xcassets", isDirectory: true)
-let appIconSetUrl = assetsUrl.appendingPathComponent("AppIcon.appiconset").appendingPathComponent("Contents.json")
+let appIconContentsUrl = assetsUrl.appendingPathComponent("AppIcon.appiconset").appendingPathComponent("Contents.json")
   //URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/Assets.xcassets/AppIcon.appiconset/Contents.json", isDirectory: true)
 
-let imageSetUrl = assetsUrl.appendingPathComponent("ImageSet.imageset").appendingPathComponent("Contents.json") //URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/Assets.xcassets/ImageSet.imageset/Contents.json", isDirectory: true)
+let imageSetContentsUrl = assetsUrl.appendingPathComponent("ImageSet.imageset").appendingPathComponent("Contents.json") //URL(fileURLWithPath: "/Users/leo/Documents/Projects/Speculid/examples/Assets/Assets.xcassets/ImageSet.imageset/Contents.json", isDirectory: true)
 
 let iconsUrl = assetsUrl.appendingPathComponent("icons")
 
@@ -64,9 +66,16 @@ for case let url as URL in enumerator {
   }
   
   let components = url.deletingPathExtension().pathComponents[filePath.pathComponents.count...]
-  let appIconSetUrl = iconsUrl.appendingPathComponent(components.joined(separator: "-")).appendingPathExtension("appiconset")
-  let imageSetUrl = iconsUrl.appendingPathComponent(components.joined(separator: "-")).appendingPathExtension("imageset")
+  let name = components.joined(separator: "-")
+  let appIconSetUrl = iconsUrl.appendingPathComponent(name).appendingPathExtension("appiconset")
+  let imageSetUrl = iconsUrl.appendingPathComponent(name).appendingPathExtension("imageset")
+  let appIconSpecUrl = speculidIconsUrl.appendingPathComponent("\(name).appicon.speculid")
+  let imageSetSpecUrl = speculidIconsUrl.appendingPathComponent("\(name).imageset.speculid")
   try! FileManager.default.createDirectory(at: appIconSetUrl, withIntermediateDirectories: false, attributes: nil)
   try! FileManager.default.createDirectory(at: imageSetUrl, withIntermediateDirectories: false, attributes: nil)
   
+  try! FileManager.default.copyItem(at: appIconContentsUrl, to: appIconSetUrl.appendingPathComponent("Contents.json"))
+  try! FileManager.default.copyItem(at: imageSetContentsUrl, to: imageSetUrl.appendingPathComponent("Contents.json"))
+  try! FileManager.default.copyItem(at: appIconSpeculidUrl, to: appIconSpecUrl)
+    try! FileManager.default.copyItem(at: imageSetSpeculidUrl, to: imageSetSpecUrl)
 }
