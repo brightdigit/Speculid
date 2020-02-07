f231dd624b6ec9698eef14dfe80ede9e80ee1a88
Leo Dion <leogdion@brightdigit.com>
2020-02-06 20:58:35 -0500

    Revert "working variable replace"
    This reverts commit d2381b70786083022e7c402dbbff9ced104926fd.

--- setup.sh
+++ setup.sh
@@ -9,16 +9,16 @@ USER_NAME=`basename $(dirname $(git remote get-url origin))`
 #LC_ALL=C find . -type f -exec sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" {} +
 #grep -rl _PACKAGE_NAME . | xargs sed -i "s/_PACKAGE_NAME/$PACKAGE_NAME/g"
 #find . -type f -name "*" -print0 | xargs -0 sed -i '' -e 's/_PACKAGE_NAME/${PACKAGE_NAME}/g'
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" .github/workflows/macOS.yml
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" .github/workflows/ubuntu.yml
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" .travis.yml
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" README.md
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" _PACKAGE_NAME.podspec
-sed -i '' "s/_PACKAGE_NAME/$PACKAGE_NAME/g" LICENSE
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' .github/workflows/macOS.yml
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' .github/workflows/ubuntu.yml
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' .travis.yml
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' README.md
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' _PACKAGE_NAME.podspec
+sed -i '' 's/_PACKAGE_NAME/$PACKAGE_NAME/g' LICENSE
 
-sed -i '' "s/_USER_NAME/$USER_NAME/g" .github/workflows/macOS.yml
-sed -i '' "s/_USER_NAME/$USER_NAME/g" .github/workflows/ubuntu.yml
-sed -i '' "s/_USER_NAME/$USER_NAME/g" .travis.yml
-sed -i '' "s/_USER_NAME/$USER_NAME/g" README.md
-sed -i '' "s/_USER_NAME/$USER_NAME/g" _PACKAGE_NAME.podspec
-sed -i '' "s/_USER_NAME/$USER_NAME/g" LICENSE
+sed -i '' 's/_USER_NAME/$USER_NAME/g' .github/workflows/macOS.yml
+sed -i '' 's/_USER_NAME/$USER_NAME/g' .github/workflows/ubuntu.yml
+sed -i '' 's/_USER_NAME/$USER_NAME/g' .travis.yml
+sed -i '' 's/_USER_NAME/$USER_NAME/g' README.md
+sed -i '' 's/_USER_NAME/$USER_NAME/g' _PACKAGE_NAME.podspec
+sed -i '' 's/_USER_NAME/$USER_NAME/g' LICENSE
