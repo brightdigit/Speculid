a38063ca46deb492ef7d9b8e844259683e614b55
Leo Dion <leogdion@brightdigit.com>
2020-02-07 09:18:49 -0500

    using pod spec create

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ sed -i '' "s/_USER_NAME/$USER_NAME/g" README.md
 sed -i '' "s/_USER_NAME/$USER_NAME/g" _PACKAGE_NAME.podspec
 sed -i '' "s/_USER_NAME/$USER_NAME/g" LICENSE
 
-pod spec create $(git remote get-url origin)
+pod spec create $(git remote get-url origin) --silent
 
 sed -i '' 's|spec.source_files  =.*|spec.source_files  = "Sources/**/*.swift"|g' $PACKAGE_NAME.podspec 
 sed -i '' 's|spec.exclude_files.*|spec.swift_versions = "5"|g' $PACKAGE_NAME.podspec 
