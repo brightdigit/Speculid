6ae3e40b2357bd3f30b99315d0f9c9238d013b03
Leo Dion <leogdion@brightdigit.com>
2020-03-30 19:43:39 -0400

    working on syncing values

--- Speculid/ContentView.swift
+++ Speculid/ContentView.swift
@@ -1,9 +1,28 @@
 import SwiftUI
 
+extension UserDefaults {
+  @objc dynamic var count: Int {
+    get {
+      return self.integer(forKey: "count")
+    }
+    set {
+      self.set(newValue, forKey: "count")
+    }
+  }
+}
+class DefaultsObject : ObservableObject {
+  let defaults : UserDefaults! = UserDefaults.init(suiteName: "MLT7M394S7.group.com.brightdigit.Speculid")
+  
+  @Published var count : Int = 0
+  
+  init () {
+    defaults.publisher(for: \.count).assign(to: \DefaultsObject.count, on: self)
+  }
+}
 struct ContentView: View {
+  var defaultsObject = DefaultsObject()
   var body: some View {
-    Text("Hello, World!")
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
+    Text("\(self.defaultsObject.count)").frame(maxWidth: .infinity, maxHeight: .infinity)
   }
 }
 
--- Speculid/main.swift
+++ Speculid/main.swift
@@ -1,3 +1,8 @@
 import Foundation
 
-print("Hello, World!")
+guard let defaults = UserDefaults.init(suiteName: "MLT7M394S7.group.com.brightdigit.Speculid") else {
+  fatalError("No defaults")
+}
+
+let count = defaults.integer(forKey: "count")
+defaults.set(count + 1, forKey: "count")
