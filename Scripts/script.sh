05e51552f5c43be9af5d6775dc30311ddcaf5620
Leo Dion <leogdion@brightdigit.com>
2020-03-30 19:19:36 -0400

    fixing travis script

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -14,15 +14,15 @@ fi
 #swift build
 #swift test  --enable-code-coverage
 
-if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+#if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   #xcrun llvm-cov export -format="lcov" .build/debug/${FRAMEWORK_NAME}PackageTests.xctest/Contents/MacOS/${FRAMEWORK_NAME}PackageTests -instr-profile .build/debug/codecov/default.profdata > info.lcov
   #bash <(curl https://codecov.io/bash) -F travis -F macOS -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
-else
+#else
   #llvm-cov export -format="lcov" .build/x86_64-unknown-linux-gnu/debug/${FRAMEWORK_NAME}PackageTests.xctest -instr-profile .build/debug/codecov/default.profdata > info.lcov
   #bash <(curl https://codecov.io/bash) -F travis -F bionic -n $TRAVIS_JOB_NUMBER-$TRAVIS_OS_NAME
-fi
+#fi
 
-if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+#if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   #swift package dump-package | jq -e ".products | length > 0"
   #pod lib lint
   #swift package generate-xcodeproj
@@ -30,4 +30,4 @@ if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
   #xcodebuild -workspace Example/Example.xcworkspace -scheme "iOS_Example"  ONLY_ACTIVE_ARCH=NO  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
   #xcodebuild -workspace Example/Example.xcworkspace -scheme "tvOS_Example"  ONLY_ACTIVE_ARCH=NO   CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
   #xcodebuild -workspace Example/Example.xcworkspace -scheme "macOS_Example"  ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO  CODE_SIGNING_ALLOWED=NO
-fi
+#fi
