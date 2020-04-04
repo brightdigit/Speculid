0407ec71f728b65ff53f64993122c911b9ea5855
Leo Dion <leogdion@brightdigit.com>
2020-04-03 22:09:17 -0400

    working on file copy operation

--- Speculid/AppDelegate.swift
+++ Speculid/AppDelegate.swift
@@ -1,9 +1,16 @@
 import Cocoa
+import SpeculidKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_: Notification) {
     // Insert code here to initialize your application
+    
+    do {
+    try FileTesting.copyFile(from: URL(fileURLWithPath: #file), to: URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("AppDelegate.copy"))
+    } catch {
+      debugPrint(error)
+    }
   }
 
   func applicationWillTerminate(_: Notification) {
