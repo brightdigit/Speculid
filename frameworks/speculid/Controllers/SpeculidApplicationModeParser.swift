ae6700127a69861398ee19c4a9f25d8c017c5979
Leo Dion <leogdion@brightdigit.com>
2020-04-13 16:22:17 -0400

    refactored code

--- frameworks/speculid/Controllers/SpeculidApplicationModeParser.swift
+++ frameworks/speculid/Controllers/SpeculidApplicationModeParser.swift
@@ -1,6 +1,31 @@
 import Foundation
 
 public struct SpeculidApplicationModeParser: SpeculidApplicationModeParserProtocol {
+  private func parseInitializeArguments(_ arguments: [String]) -> SpeculidCommandArgumentSet? {
+    let urls = arguments.compactMap { filePath -> URL? in
+      guard FileManager.default.fileExists(atPath: filePath) || filePath.components(separatedBy: ".").last?.lowercased() == "speculid" else {
+        return nil
+      }
+      return URL(fileURLWithPath: filePath)
+    }
+    let types = ["json": "asset", "svg": "image", "appiconset": "asset", "imageset": "asset", "png": "image", "speculid": "destination"]
+    let urlTypes = urls.compactMap { url -> (String, URL)? in
+      guard let type = types[url.pathExtension.lowercased()] else {
+        return nil
+      }
+      return (type, url)
+    }
+    let dictionary = [String: [(String, URL)]].init(grouping: urlTypes) {
+      $0.0
+    }.compactMapValues {
+      $0.first?.1
+    }
+    if let asset = dictionary["asset"], let image = dictionary["image"], let destination = dictionary["destination"] {
+      return .initialize(asset: asset, image: image, destination: destination)
+    } else {
+      return nil
+    }
+  }
   public func parseMode(fromCommandLine commandLine: CommandLineArgumentProviderProtocol) -> SpeculidApplicationMode {
     var indicies = [Int]()
     if commandLine.arguments.first == Bundle.main.executablePath {
@@ -32,30 +57,8 @@ public struct SpeculidApplicationModeParser: SpeculidApplicationModeParserProtoc
       } else if arguments.contains("--install") {
         return .command(.install(.all))
       } else if let index = arguments.firstIndex(of: "--initialize") {
-        let urls = arguments[arguments.index(after: index)...].compactMap {
-          filePath -> URL? in
-          guard FileManager.default.fileExists(atPath: filePath) || filePath.components(separatedBy: ".").last?.lowercased() == "speculid" else {
-            return nil
-          }
-          return URL(fileURLWithPath: filePath)
-        }
-        let types = ["json": "asset", "svg": "image", "appiconset": "asset", "imageset": "asset", "png": "image", "speculid": "destination"]
-        let urlTypes = urls.compactMap {
-          url -> (String, URL)? in
-          guard let type = types[url.pathExtension.lowercased()] else {
-            return nil
-          }
-          return (type, url)
-        }
-        let dictionary = [String: [(String, URL)]].init(grouping: urlTypes) {
-          $0.0
-        }.compactMapValues {
-          $0.first?.1
-        }
-        guard let asset = dictionary["asset"], let image = dictionary["image"], let destination = dictionary["destination"] else {
-          return .command(.unknown(arguments))
-        }
-        return .command(.initialize(asset: asset, image: image, destination: destination))
+        let commandArgs = parseInitializeArguments([String](arguments[arguments.index(after: index)...]))
+        return .command(commandArgs ?? SpeculidCommandArgumentSet.unknown(arguments))
       } else {
         return .command(.unknown(arguments))
       }
