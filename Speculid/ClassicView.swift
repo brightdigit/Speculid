66e1b4b0e296639d136c546469891f103a979e1d
Leo Dion <leogdion@brightdigit.com>
2020-07-16 21:38:48 -0400

    migrating to new UI

--- Speculid/ClassicView.swift
+++ Speculid/ClassicView.swift
@@ -80,6 +80,14 @@ class ClassicObject : ObservableObject {
     self.url?.appendingPathComponent(self.sourceImageRelativePath)
   }
   
+  var isAppIcon : Bool {
+    self.assetDirectoryRelativePath.lowercased().hasSuffix(".appiconset")
+  }
+  
+  var isImageSet : Bool {
+    self.assetDirectoryRelativePath.lowercased().hasSuffix(".imageset")
+  }
+  
   init (url: URL?, document: ClassicDocument) {
     self.url = url
     self.document = document
@@ -103,7 +111,7 @@ class ClassicObject : ObservableObject {
         return Geometry(value: value, dimension: .width)
       case .some(.height):
         return Geometry(value: value, dimension: .height)
-        default:
+      default:
         return nil
       }
     }
@@ -138,50 +146,94 @@ struct ClassicView: View {
       
       self.bookmarkCollection.isAvailable(basedOn: self.object.url, relativePath: self.object.document.document.sourceImageRelativePath)
   }
+  
   var body: some View {
-    VStack{
-      Button(action: {
-        self.importFiles(singleOfType: [.svg, .png]) { (result) in
-          guard case let .success(url) = result else {
-            return
-          }
-          bookmarkCollection.saveBookmark(url)
-        }
-      }, label: {
+    Form{
+      Section(header: Text("Source Graphic")){
         HStack{
-          Image(systemName: self.bookmarkCollection.isAvailable(basedOn: self.object.url, relativePath: self.object.document.document.sourceImageRelativePath) ? "lock.open.fill" : "lock.fill")
-          Image(systemName: "photo.fill")
-          Text(self.object.sourceImageRelativePath)
+          Spacer()
+          Text("File Path:").frame(width: 75, alignment: .trailing)
+          TextField("SVG of PNG File", text: self.$object.sourceImageRelativePath)
+            .overlay(Image(systemName: "folder.fill").foregroundColor(.primary).padding(.trailing, 4.0), alignment: .trailing)
+            .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/).frame(width: 250).onTapGesture {
+              self.importFiles(singleOfType: [.svg, .png]) { (result) in
+                guard case let .success(url) = result else {
+                  return
+                }
+                bookmarkCollection.saveBookmark(url)
+              }
+            }
+          Image(systemName: "lock.fill").foregroundColor(.yellow).opacity(self.bookmarkCollection.isAvailable(basedOn: self.object.url, relativePath: self.object.sourceImageRelativePath) ? 0.0 : 1.0)
+          Spacer()
         }
-      })
-      
-      Button(action: {
-        
-        self.importFiles(singleOfType: [.directory]) { (result) in
-          guard case let .success(url) = result else {
-            return
-          }
-          bookmarkCollection.saveBookmark(url)
-        }
-      }, label: {
+      }
+      Divider()
+      Section(header: Text("Asset Catalog")){
         HStack{
-          Image(systemName: self.bookmarkCollection.isAvailable(basedOn: self.object.url, relativePath: self.object.document.document.assetDirectoryRelativePath) ? "lock.open.fill" : "lock.fill")
-          Image(systemName: "photo.fill")
-          Text(self.object.assetDirectoryRelativePath)
+          Spacer()
+          Text("Folder:").frame(width: 75, alignment: .trailing)
+          TextField(".appiconset or .imageset", text: self.$object.assetDirectoryRelativePath)
+            .overlay(Image(systemName: "folder.fill").foregroundColor(.primary).padding(.trailing, 4.0), alignment: .trailing)
+            .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/).frame(width: 250).onTapGesture {
+              self.importFiles(singleOfType: [.directory]) { (result) in
+                guard case let .success(url) = result else {
+                  return
+                }
+                bookmarkCollection.saveBookmark(url)
+              }
+            }
+          Image(systemName: "lock.fill").foregroundColor(.yellow).opacity(self.bookmarkCollection.isAvailable(basedOn: self.object.url, relativePath: self.object.assetDirectoryRelativePath) ? 0.0 : 1.0)
+          Spacer()
         }
-      })
-      
-      
-      Toggle("Remove Alpha", isOn: self.$object.removeAlpha)
-      ColorPicker("Background Color", selection: self.$object.backgroundColor)
-      
-      Button("Build") {
-        if let url = self.object.url {
-          self.object.document.build(fromURL: url, inSandbox: self.bookmarkCollection)
+      }
+      Divider()
+      Section(header: Text("App Icon Modifications")) {
+        HStack{
+          VStack(alignment: .leading){
+            Toggle("Remove Alpha Channel", isOn: self.$object.removeAlpha)
+            Text("If this is intended for an iOS, watchOS, or tvOS App, then you should remove the alpha channel from the source graphic.").multilineTextAlignment(.leading).font(.subheadline).lineLimit(nil)
+          }
+          Spacer()
         }
-      }.disabled(!self.canBuild)
-      
-    }
+        VStack(alignment: .leading){
+          HStack{
+            Toggle("Add a Background Color", isOn: self.$object.addBackground)
+            ColorPicker("", selection: self.$object.backgroundColor, supportsOpacity: false).labelsHidden().frame(width: 40, height: 25, alignment: .trailing).disabled(!self.object.addBackground).opacity(self.object.addBackground ? 1.0 : 0.5)
+          }
+          Text("If this is intended for an iOS, watchOS, or tvOS App, then you should set a background color.").multilineTextAlignment(.leading).font(.subheadline).lineLimit(nil)
+        }
+      }.disabled(!self.object.isAppIcon).opacity(self.object.isAppIcon ? 1.0 : 0.5)
+      Divider()
+      Section(header: Text("Resizing Geometry")) {
+        HStack{
+          Picker("Resize", selection: self.$object.resizeOption) {
+            ForEach(ResizeOption.all) {
+              Text($0.label).tag($0.rawValue)
+            }
+          }.pickerStyle(SegmentedPickerStyle())
+          .frame(width: 150, alignment: .leading).labelsHidden()
+          TextField("Value", value: self.$object.geometryValue, formatter: NumberFormatter()).frame(width: 50, alignment: .leading).disabled(self.object.resizeOption == 0).opacity(self.object.resizeOption == 0 ? 0.5 : 1.0)
+          Text("px").opacity(self.object.resizeOption == 0 ? 0.5 : 1.0)
+        }
+        Text("If you wish to render scaled PNG files for an image set, then specify either width or height and the image will be resized to that dimention while retaining its aspect ratio.\nOtherwise if you select \"None\", then only a PDF will be rendered.   ").multilineTextAlignment(.leading).font(.subheadline).lineLimit(nil)
+      }.disabled(!self.object.isImageSet).opacity(self.object.isImageSet ? 1.0 : 0.5)
+      Divider()
+      Section{
+        HStack{
+          Button {
+            if let url = self.object.url {
+              self.object.document.build(fromURL: url, inSandbox: self.bookmarkCollection)
+            }
+          } label: {
+            HStack{
+              Image(systemName: "play.fill")
+              Text("Build")
+            }
+          }
+
+        }
+      }
+    }.padding(.all, 40.0).frame(minWidth: 500, idealWidth: 500, maxWidth: 600, minHeight: 500, idealHeight: 500, maxHeight: .infinity, alignment: .center)
   }
 }
 
